0609f708015e3863d3b321c0580f37f1c344f880
Ryosuke Tsuchida <j3350pai@sit.ac.jp>
2025-12-17 12:21:12 +0900

    feat: Add instant respawn and debug key

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let gameMode = 'normal'; // 'normal', 'boss_intro', 'boss_battle', 'game_clear'
 let scoreSinceLastBoss = 0;
 let boss = null;
 let bossLevel = 1;
-let bossesDefeated = 0; // Counter for defeated bosses
+let bossesDefeated = 0;
 let gameClearParticles = [];
 
 // --- Player ---
@@ -23,6 +23,8 @@ class Player {
         this.speed = 7;
         this.shootCooldown = 0;
         this.hue = 0;
+        this.invincible = false;
+        this.invincibilityTimer = 0;
     }
 
     update() {
@@ -36,10 +38,19 @@ class Player {
             this.shootCooldown = 10;
         }
         if (this.shootCooldown > 0) this.shootCooldown--;
+        
+        if (this.invincibilityTimer > 0) {
+            this.invincibilityTimer--;
+        } else {
+            this.invincible = false;
+        }
         this.hue = (this.hue + 1) % 360;
     }
 
     draw() {
+        if (this.invincible && Math.floor(this.invincibilityTimer / 10) % 2 === 0) {
+            return; // Blinking effect
+        }
         ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
@@ -53,120 +64,48 @@ class Player {
         const bulletColor = `hsl(${this.hue}, 100%, 50%)`;
         bullets.push(new Bullet(this.x + this.width, this.y + this.height / 2, bulletColor));
     }
+    
+    startInvincibility() {
+        this.invincible = true;
+        this.invincibilityTimer = 120; // 2 seconds at 60fps
+    }
 }
 
 // --- Bullets ---
 class Bullet {
-    constructor(x, y, color) {
-        this.x = x; this.y = y; this.width = 15; this.height = 5; this.speed = 10; this.color = color;
-    }
+    constructor(x, y, color) { this.x = x; this.y = y; this.width = 15; this.height = 5; this.speed = 10; this.color = color; }
     update() { this.x += this.speed; }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y - this.height / 2, this.width, this.height);
-    }
+    draw() { ctx.fillStyle = this.color; ctx.fillRect(this.x, this.y - this.height / 2, this.width, this.height); }
 }
 
 class EnemyBullet {
-    constructor(x, y, speedY = 0) {
-        this.x = x; this.y = y; this.width = 10; this.height = 10; this.speedX = -5; this.speedY = speedY;
-    }
-    update() { 
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
-    draw() {
-        ctx.fillStyle = 'magenta';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
+    constructor(x, y, speedY = 0) { this.x = x; this.y = y; this.width = 10; this.height = 10; this.speedX = -5; this.speedY = speedY; }
+    update() { this.x += this.speedX; this.y += this.speedY; }
+    draw() { ctx.fillStyle = 'magenta'; ctx.fillRect(this.x, this.y, this.width, this.height); }
 }
 
 // --- Enemies ---
 class Enemy {
-    constructor() {
-        this.x = canvas.width; this.y = Math.random() * (canvas.height - 40) + 20; this.width = 30; this.height = 30; this.speedX = Math.random() * 2 + 1;
-    }
+    constructor() { this.x = canvas.width; this.y = Math.random() * (canvas.height - 40) + 20; this.width = 30; this.height = 30; this.speedX = Math.random() * 2 + 1; }
     update() { this.x -= this.speedX; }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
+    draw() { ctx.fillStyle = this.color; ctx.fillRect(this.x, this.y, this.width, this.height); }
 }
 class EnemyType1 extends Enemy { constructor() { super(); this.color = '#ff4d4d'; } }
-class EnemyType2 extends Enemy {
-    constructor() { super(); this.color = '#4da6ff'; this.angle = 0; this.amplitude = Math.random() * 3 + 1; }
-    update() { super.update(); this.angle += 0.05; this.y += Math.sin(this.angle) * this.amplitude; }
-}
-class EnemyType3 extends Enemy {
-    constructor() { super(); this.color = '#66ff66'; this.speedX = Math.random() * 3 + 3; this.speedY = Math.random() * 5 + 3; if (Math.random() < 0.5) this.speedY = -this.speedY; }
-    update() { super.update(); this.y += this.speedY; if (this.y <= 0 || this.y >= canvas.height - this.height) { this.speedY = -this.speedY; } }
-}
+class EnemyType2 extends Enemy { constructor() { super(); this.color = '#4da6ff'; this.angle = 0; this.amplitude = Math.random() * 3 + 1; } update() { super.update(); this.angle += 0.05; this.y += Math.sin(this.angle) * this.amplitude; } }
+class EnemyType3 extends Enemy { constructor() { super(); this.color = '#66ff66'; this.speedX = Math.random() * 3 + 3; this.speedY = Math.random() * 5 + 3; if (Math.random() < 0.5) this.speedY = -this.speedY; } update() { super.update(); this.y += this.speedY; if (this.y <= 0 || this.y >= canvas.height - this.height) { this.speedY = -this.speedY; } } }
 
 // --- Boss ---
 class Boss {
-    constructor() {
-        this.x = canvas.width; this.y = canvas.height / 2 - 50; this.width = 100; this.height = 100; this.speedX = 2; this.speedY = 2; this.hp = 150; this.maxHp = this.hp; this.introState = 'entering'; this.shootCooldown = 0; this.shootInterval = 90;
-    }
-    update() {
-        if (this.introState === 'entering') {
-            this.x -= this.speedX;
-            if (this.x <= canvas.width - this.width - 50) { this.x = canvas.width - this.width - 50; this.introState = 'active'; }
-        } else if (this.introState === 'active') {
-            this.y += this.speedY;
-            if (this.y <= 0 || this.y >= canvas.height - this.height) { this.speedY *= -1; }
-            if (this.shootCooldown <= 0) { this.shoot(); this.shootCooldown = this.shootInterval; } else { this.shootCooldown--; }
-        }
-    }
-    draw() {
-        ctx.fillStyle = 'purple'; ctx.fillRect(this.x, this.y, this.width, this.height);
-        const hpBarWidth = (this.hp / this.maxHp) * this.width;
-        ctx.fillStyle = 'red'; ctx.fillRect(this.x, this.y - 15, this.width, 10);
-        ctx.fillStyle = 'lime'; ctx.fillRect(this.x, this.y - 15, hpBarWidth, 10);
-    }
-    shoot() {
-        const bulletY = this.y + this.height / 2;
-        const numBullets = bossLevel;
-        if (numBullets === 1) {
-            enemyBullets.push(new EnemyBullet(this.x, bulletY, 0));
-        } else {
-            const spread = 2;
-            for (let i = 0; i < numBullets; i++) {
-                const speedY = (i - (numBullets - 1) / 2) * spread;
-                enemyBullets.push(new EnemyBullet(this.x, bulletY, speedY));
-            }
-        }
-    }
-    takeDamage(amount) {
-        this.hp -= amount;
-        if (this.hp <= 0) { this.hp = 0; return true; }
-        return false;
-    }
+    constructor() { this.x = canvas.width; this.y = canvas.height / 2 - 50; this.width = 100; this.height = 100; this.speedX = 2; this.speedY = 2; this.hp = 150; this.maxHp = this.hp; this.introState = 'entering'; this.shootCooldown = 0; this.shootInterval = 90; }
+    update() { if (this.introState === 'entering') { this.x -= this.speedX; if (this.x <= canvas.width - this.width - 50) { this.x = canvas.width - this.width - 50; this.introState = 'active'; } } else if (this.introState === 'active') { this.y += this.speedY; if (this.y <= 0 || this.y >= canvas.height - this.height) { this.speedY *= -1; } if (this.shootCooldown <= 0) { this.shoot(); this.shootCooldown = this.shootInterval; } else { this.shootCooldown--; } } }
+    draw() { ctx.fillStyle = 'purple'; ctx.fillRect(this.x, this.y, this.width, this.height); const hpBarWidth = (this.hp / this.maxHp) * this.width; ctx.fillStyle = 'red'; ctx.fillRect(this.x, this.y - 15, this.width, 10); ctx.fillStyle = 'lime'; ctx.fillRect(this.x, this.y - 15, hpBarWidth, 10); }
+    shoot() { const bulletY = this.y + this.height / 2; const numBullets = bossLevel; if (numBullets === 1) { enemyBullets.push(new EnemyBullet(this.x, bulletY, 0)); } else { const spread = 2; for (let i = 0; i < numBullets; i++) { const speedY = (i - (numBullets - 1) / 2) * spread; enemyBullets.push(new EnemyBullet(this.x, bulletY, speedY)); } } }
+    takeDamage(amount) { this.hp -= amount; if (this.hp <= 0) { this.hp = 0; return true; } return false; }
 }
 
 // --- Effects ---
-class Star {
-    constructor() { this.x = Math.random() * canvas.width; this.y = Math.random() * canvas.height; this.size = Math.random() * 2 + 0.5; this.speed = Math.random() * 1.5 + 0.5; }
-    update() { this.x -= this.speed; if (this.x < 0) { this.x = canvas.width; this.y = Math.random() * canvas.height; } }
-    draw() { ctx.fillStyle = 'white'; ctx.fillRect(this.x, this.y, this.size, this.size); }
-}
-
-class GameClearParticle {
-    constructor() {
-        this.x = canvas.width / 2; this.y = canvas.height / 2;
-        this.radius = Math.random() * 5 + 1;
-        this.color = `hsl(${Math.random() * 360}, 100%, 70%)`;
-        const angle = Math.random() * Math.PI * 2;
-        const speed = Math.random() * 8;
-        this.vx = Math.cos(angle) * speed;
-        this.vy = Math.sin(angle) * speed;
-        this.alpha = 1;
-    }
-    update() { this.x += this.vx; this.y += this.vy; this.alpha -= 0.01; }
-    draw() {
-        ctx.save(); ctx.globalAlpha = this.alpha; ctx.fillStyle = this.color; ctx.beginPath(); ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); ctx.fill(); ctx.restore();
-    }
-}
-
+class Star { constructor() { this.x = Math.random() * canvas.width; this.y = Math.random() * canvas.height; this.size = Math.random() * 2 + 0.5; this.speed = Math.random() * 1.5 + 0.5; } update() { this.x -= this.speed; if (this.x < 0) { this.x = canvas.width; this.y = Math.random() * canvas.height; } } draw() { ctx.fillStyle = 'white'; ctx.fillRect(this.x, this.y, this.size, this.size); } }
+class GameClearParticle { constructor() { this.x = canvas.width / 2; this.y = canvas.height / 2; this.radius = Math.random() * 5 + 1; this.color = `hsl(${Math.random() * 360}, 100%, 70%)`; const angle = Math.random() * Math.PI * 2; const speed = Math.random() * 8; this.vx = Math.cos(angle) * speed; this.vy = Math.sin(angle) * speed; this.alpha = 1; } update() { this.x += this.vx; this.y += this.vy; this.alpha -= 0.01; } draw() { ctx.save(); ctx.globalAlpha = this.alpha; ctx.fillStyle = this.color; ctx.beginPath(); ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); ctx.fill(); ctx.restore(); } }
 
 // --- Game Objects Arrays ---
 const player = new Player();
@@ -174,24 +113,15 @@ const bullets = []; const enemyBullets = []; const enemies = []; const stars = [
 function init() { for (let i = 0; i < 100; i++) stars.push(new Star()); }
 
 // --- Game Logic ---
-function resetGameRound() {
-    player.x = 50; player.y = canvas.height / 2;
-    bullets.length = 0; enemyBullets.length = 0; enemies.length = 0;
-    boss = null; gameMode = 'normal'; scoreSinceLastBoss = 0; enemyTimer = 0;
+function clearThreats() {
+    enemyBullets.length = 0;
+    enemies.length = 0;
+    // Don't clear the boss, just bullets and enemies
 }
 
 let enemyTimer = 0;
 const enemyInterval = 70;
-function handleEnemies() {
-    if (gameMode !== 'normal') return;
-    if (enemyTimer > enemyInterval) {
-        const enemyType = Math.floor(Math.random() * 3);
-        if (enemyType === 0) enemies.push(new EnemyType1());
-        else if (enemyType === 1) enemies.push(new EnemyType2());
-        else enemies.push(new EnemyType3());
-        enemyTimer = 0;
-    } else { enemyTimer++; }
-}
+function handleEnemies() { if (gameMode !== 'normal') return; if (enemyTimer > enemyInterval) { const enemyType = Math.floor(Math.random() * 3); if (enemyType === 0) enemies.push(new EnemyType1()); else if (enemyType === 1) enemies.push(new EnemyType2()); else enemies.push(new EnemyType3()); enemyTimer = 0; } else { enemyTimer++; } }
 
 function handleCollisions() {
     // Player bullets vs Enemies
@@ -218,7 +148,8 @@ function handleCollisions() {
                         gameMode = 'game_clear';
                         for (let k = 0; k < 200; k++) gameClearParticles.push(new GameClearParticle());
                     } else {
-                        resetGameRound();
+                        gameMode = 'normal'; // Go back to normal mode
+                        boss = null;
                     }
                 }
             }
@@ -226,14 +157,21 @@ function handleCollisions() {
     }
 
     // Player vs Dangers
-    let playerHit = false;
-    for (const e of enemies) { if (player.x < e.x + e.width && player.x + player.width > e.x && player.y < e.y + e.height && player.y + player.height > e.y) { playerHit = true; break; } }
-    if (!playerHit && boss && gameMode === 'boss_battle' && player.x < boss.x + boss.width && player.x + player.width > boss.x && player.y < boss.y + boss.height && player.y + player.height > boss.y) { playerHit = true; }
-    if (!playerHit) { for (const eb of enemyBullets) { if (player.x < eb.x + eb.width && player.x + player.width > eb.x && player.y < eb.y + eb.height && player.y + player.height > eb.y) { playerHit = true; break; } } }
+    if (!player.invincible) {
+        let playerHit = false;
+        for (const e of enemies) { if (player.x < e.x + e.width && player.x + player.width > e.x && player.y < e.y + e.height && player.y + player.height > e.y) { playerHit = true; break; } }
+        if (!playerHit && boss && gameMode === 'boss_battle' && player.x < boss.x + boss.width && player.x + player.width > boss.x && player.y < boss.y + boss.height && player.y + player.height > boss.y) { playerHit = true; }
+        if (!playerHit) { for (const eb of enemyBullets) { if (player.x < eb.x + eb.width && player.x + player.width > eb.x && player.y < eb.y + eb.height && player.y + player.height > eb.y) { playerHit = true; break; } } }
 
-    if (playerHit) {
-        lives--;
-        if (lives <= 0) { gameOver = true; } else { resetGameRound(); }
+        if (playerHit) {
+            lives--;
+            clearThreats();
+            if (lives <= 0) {
+                gameOver = true;
+            } else {
+                player.startInvincibility();
+            }
+        }
     }
 }
 
@@ -242,17 +180,32 @@ function startBossBattle() {
     setTimeout(() => { gameMode = 'boss_battle'; }, 2000);
 }
 
+function handleDebugKeys() {
+    if (keys['c'] || keys['C']) {
+        bossLevel = 3;
+        bossesDefeated = 2;
+        gameMode = 'normal';
+        scoreSinceLastBoss = 100; // This will trigger the boss battle in the next frame
+        clearThreats();
+        boss = null;
+        keys['c'] = false; // Prevent continuous trigger
+        keys['C'] = false;
+    }
+}
+
 // --- Main Game Loop ---
 function animate() {
     if (gameOver) {
         ctx.fillStyle = "white"; ctx.font = "50px sans-serif"; ctx.textAlign = "center";
         ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
-        ctx.font = "30px sans-serif"; ctx.fillText(`Final Score: ${score}`, canvas.width/2, canvas.height/2 + 40);
+        ctx.font = "30px sans-serif"; ctx.fillText(`Final Score: ${score}`, canvas.width / 2, canvas.height / 2 + 40);
         return;
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     stars.forEach(s => { s.update(); s.draw(); });
+    
+    handleDebugKeys();
 
     switch (gameMode) {
         case 'normal':
@@ -269,7 +222,7 @@ function animate() {
             break;
 
         case 'game_clear':
-            gameClearParticles.forEach((p, i) => { p.update(); p.draw(); if(p.alpha <= 0) gameClearParticles.splice(i, 1); });
+            gameClearParticles.forEach((p, i) => { p.update(); p.draw(); if (p.alpha <= 0) gameClearParticles.splice(i, 1); });
             ctx.font = "70px sans-serif"; ctx.textAlign = "center";
             const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
             gradient.addColorStop(0, `hsl(${(player.hue) % 360}, 100%, 70%)`);
@@ -285,7 +238,7 @@ function animate() {
     ctx.fillStyle = "white"; ctx.font = "20px sans-serif"; ctx.textAlign = "left";
     ctx.fillText(`Score: ${score}`, 10, 25);
     ctx.fillText(`Lives: ${lives}`, 10, 50);
-    ctx.fillText(`Boss Level: ${bossLevel}`, 10, 75);
+    ctx.fillText(`Bosses Defeated: ${bossesDefeated}`, 10, 75);
     
     if (gameMode === 'boss_intro' || gameMode === 'boss_battle') {
         ctx.font = "30px sans-serif"; ctx.textAlign = "center"; ctx.fillStyle = "red";
@@ -296,8 +249,8 @@ function animate() {
 }
 
 // --- Event Listeners ---
-window.addEventListener('keydown', (e) => { keys[e.key] = true; });
-window.addEventListener('keyup', (e) => { keys[e.key] = false; });
+window.addEventListener('keydown', (e) => { keys[e.key.toLowerCase()] = true; });
+window.addEventListener('keyup', (e) => { keys[e.key.toLowerCase()] = false; });
 
 init();
 animate();
